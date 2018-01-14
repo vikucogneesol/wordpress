b6a9f27b7051a68de375c5ddb328e1c7cb385132
testdemo <vikram@cogneesol.com>
2018-01-14 17:20:23 +0530

    commit by vikram

--- wp-mail.php
+++ wp-mail.php
@@ -6,7 +6,7 @@
  *
  * @package WordPress
  */
- print "hello";
+ print "hello vikram ";
 /** Make sure that the WordPress bootstrap has run before continuing. */
 require(dirname(__FILE__) . '/wp-load.php');
  print "Hello How are You.";
