14da9bd3686a5e997fbbc9bf69a21b5838e6e61b
testdemo <vikram@cogneesol.com>
2018-01-14 17:19:00 +0530

    wefqwrwqer

--- wp-mail.php
+++ wp-mail.php
@@ -6,7 +6,7 @@
  *
  * @package WordPress
  */
-
+ print "hello";
 /** Make sure that the WordPress bootstrap has run before continuing. */
 require(dirname(__FILE__) . '/wp-load.php');
  print "Hello How are You.";
